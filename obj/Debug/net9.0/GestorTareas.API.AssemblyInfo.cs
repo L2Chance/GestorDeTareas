01c809fb3f22b6965df9f670c571e953294565ed
L2Chance <Estudiandoyavanzando@gmail.com>
2025-07-05 21:26:55 -0300

    Cambios menores

--- obj/Debug/net9.0/GestorTareas.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/GestorTareas.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestorTareas.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28b8f88d49f9521d362ba7ab9f0e41df0fca29f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestorTareas.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestorTareas.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
